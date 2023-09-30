78ace75bd06fb152dd682d9f006a7f98935cdd38
gmlstjq123 <gmlstjq123@naver.com>
2023-10-01 07:04:42 +0900

    FEAT: 회원가입 레이아웃 작성

--- app/src/main/java/com/example/airconmoa/config/BaseResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.airconmoa.config
-
-import com.google.gson.annotations.SerializedName
-
-open class BaseResponse(
-    @SerializedName("code") val code : String = "",
-    @SerializedName("msg") val msg : String = ""
-)
--- app/src/main/java/com/example/airconmoa/ui/login_user/model/LoginPostData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.airconmoa.ui.login_user.model
-
-data class LoginPostData(
-    val authId : String
-)
