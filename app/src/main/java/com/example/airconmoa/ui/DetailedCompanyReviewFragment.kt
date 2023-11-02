cd2b220270ca510ad720fbc68fa178857f0ebecc
hhj <sbs98010@naver.com>
2023-11-02 21:19:53 +0900

    Ui : 사용자가 보는 업체 상세정보 1/3

--- app/src/main/java/com/example/airconmoa/ui/DetailedCompanyReviewFragment.kt
+++ app/src/main/java/com/example/airconmoa/ui/DetailedCompanyReviewFragment.kt
@@ -2,16 +2,24 @@ package com.example.airconmoa.ui
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
 import com.example.airconmoa.config.BaseFragmentVB
 import com.example.airconmoa_android.R
 import com.example.airconmoa_android.databinding.FragmentDetailedCompanyReviewBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailedCompanyReviewFragment : BaseFragmentVB<FragmentDetailedCompanyReviewBinding>(FragmentDetailedCompanyReviewBinding::bind, R.layout.fragment_detailed_company_review) {
+    private val tabTitle= arrayListOf("상세정보","리뷰")
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val estimateDetialVPAdapter=EstimateDatailVPAdapter(this@DetailedCompanyReviewFragment)
+        binding.vpDetailedCompany.adapter=estimateDetialVPAdapter
+
+        TabLayoutMediator(binding.tabDetailedCompany,binding.vpDetailedCompany){
+                tab,position->
+            tab.text=tabTitle[position]
+        }.attach()
     }
 
 }
---
+++ app/src/main/java/com/example/airconmoa/ui/EstimateDatailVPAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.airconmoa.ui
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class EstimateDatailVPAdapter(fragment:Fragment):FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int =2
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0->EstimateSubDetailFragment()
+            1->EstimateSubReviewFragment()
+            else->EstimateSubDetailFragment()
+        }
+    }
+
+}
