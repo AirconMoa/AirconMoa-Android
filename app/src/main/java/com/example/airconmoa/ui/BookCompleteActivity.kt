c15553806f19c2e6a274105a2aab36c2f9410527
hhj <sbs98010@naver.com>
2023-11-03 23:26:52 +0900

    Ui : 사용자 업체 예약 화면 완료

--- app/src/main/java/com/example/airconmoa/ui/BookCompleteActivity.kt
+++ app/src/main/java/com/example/airconmoa/ui/BookCompleteActivity.kt
@@ -1,12 +1,18 @@
 package com.example.airconmoa.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.airconmoa_android.R
+import com.example.airconmoa.config.BaseActivityVB
+import com.example.airconmoa.ui.main_user.MainActivity
+import com.example.airconmoa_android.databinding.ActivityBookCompleteBinding
 
-class BookCompleteActivity : AppCompatActivity() {
+class BookCompleteActivity : BaseActivityVB<ActivityBookCompleteBinding>(ActivityBookCompleteBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_complete)
+        binding.homeBtn.setOnClickListener {
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
