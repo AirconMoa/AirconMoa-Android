7b43fd60f5793966a7a6352edf3fe4fbf05667c3
hhj <sbs98010@naver.com>
2023-11-05 06:20:12 +0900

    Ui : 업체 메인화면 리사이클러뷰 적용

--- app/src/main/java/com/example/airconmoa/ui/estimate_company/EstimateCompanyFragment.kt
+++ app/src/main/java/com/example/airconmoa/ui/estimate_company/EstimateCompanyFragment.kt
@@ -2,6 +2,7 @@ package com.example.airconmoa.ui.estimate_company
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.airconmoa.config.BaseFragmentVB
 import com.example.airconmoa_android.R
 import com.example.airconmoa_android.databinding.FragmentEstimateCompanyBinding
@@ -10,5 +11,27 @@ class EstimateCompanyFragment : BaseFragmentVB<FragmentEstimateCompanyBinding>(F
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val dataList:ArrayList<EstimateItemData> = arrayListOf()
+        dataList.apply {
+            add(
+                EstimateItemData("모아", "2023.09.26", "신규설치 · 신규건물", "500m")
+
+            )
+            add(
+                EstimateItemData("모아2", "2023.09.27", "신규설치 · 기존건물", "510m")
+            )
+            add(
+                EstimateItemData("모아3", "2023.10.01", "신규설치 · 기존건물", "450m")
+            )
+            add(
+                EstimateItemData("모아4", "2023.10.03", "교체", "650m")
+            )
+            add(
+                EstimateItemData("모아5", "2023.10.04", "신규설치 · 기존건물", "650m")
+            )
+        }
+
+        binding.rvMainCompany.adapter= EstimateItemDataAdapter(dataList)
+        binding.rvMainCompany.layoutManager= LinearLayoutManager(context)
     }
 }
---
+++ app/src/main/java/com/example/airconmoa/ui/estimate_company/EstimateItemDataAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.airconmoa.ui.estimate_company
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.airconmoa_android.databinding.ItemRvMainCompanyBinding
+
+
+class EstimateItemDataAdapter(private val dataList: List<EstimateItemData>?) :
+    RecyclerView.Adapter<EstimateItemDataAdapter.DataViewHolder>() {
+    inner class DataViewHolder(private val viewBinding: ItemRvMainCompanyBinding) :
+        RecyclerView.ViewHolder(viewBinding.root) {
+        private val context = viewBinding.root.context
+
+        fun bind(data: EstimateItemData) {
+
+            viewBinding.clientNameTv.text=data.clientName
+            viewBinding.dateTv.text=data.dateTv
+            viewBinding.contentTv1.text=data.content
+            viewBinding.contentTv1.text=data.length
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+        val binding = ItemRvMainCompanyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DataViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        holder.bind(dataList!!.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return dataList!!.size
+    }
+
+
+}
