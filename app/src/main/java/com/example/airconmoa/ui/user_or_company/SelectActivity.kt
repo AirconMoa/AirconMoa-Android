461d0d989fd5a944df89abf3431067b4058d5054
hhj <sbs98010@naver.com>
2023-11-04 23:28:57 +0900

    Ui : 업체메인화면 진행중

--- app/src/main/java/com/example/airconmoa/ui/user_or_company/SelectActivity.kt
+++ app/src/main/java/com/example/airconmoa/ui/user_or_company/SelectActivity.kt
@@ -3,6 +3,7 @@ package com.example.airconmoa.ui.user_or_company
 import android.content.Intent
 import android.os.Bundle
 import com.example.airconmoa.config.BaseActivityVB
+import com.example.airconmoa.ui.main_company.MainCompanyActivity
 import com.example.airconmoa.ui.main_user.MainActivity
 import com.example.airconmoa_android.databinding.ActivitySelectBinding
 
@@ -10,7 +11,7 @@ class SelectActivity : BaseActivityVB<ActivitySelectBinding>(ActivitySelectBindi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.companyBtn.setOnClickListener{
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, MainCompanyActivity::class.java))
 
         }
         binding.userBtn.setOnClickListener {
