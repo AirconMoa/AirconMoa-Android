de16316dd584569d1572cb50e6df7925ddf94e68
hhj <sbs98010@naver.com>
2023-11-04 17:29:24 +0900

    Ui : 홈과 내 견적 마무리 끝

---
+++ app/src/main/java/com/example/airconmoa/ui/home/UserHomeChkFragment .kt
@@ -0,0 +1,17 @@
+package com.example.airconmoa.ui.home
+
+import android.os.Bundle
+import android.view.View
+import com.example.airconmoa.config.BaseFragmentVB
+import com.example.airconmoa_android.R
+import com.example.airconmoa_android.databinding.FragmentUserHomeChkBinding
+
+class UserHomeChkFragment : BaseFragmentVB<FragmentUserHomeChkBinding>(FragmentUserHomeChkBinding::bind, R.layout.fragment_user_home_chk) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val dataList:ArrayList<CompanyItemData> = arrayListOf()
+
+    }
+
+}
---
+++ app/src/main/java/com/example/airconmoa/ui/home/UserHomeRepairlFragment.kt
@@ -0,0 +1,16 @@
+package com.example.airconmoa.ui.home
+
+import android.os.Bundle
+import android.view.View
+import com.example.airconmoa.config.BaseFragmentVB
+import com.example.airconmoa_android.R
+import com.example.airconmoa_android.databinding.FragmentUserHomeRepairlBinding
+
+class UserHomeRepairlFragment : BaseFragmentVB<FragmentUserHomeRepairlBinding>(FragmentUserHomeRepairlBinding::bind, R.layout.fragment_user_home_repairl) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
