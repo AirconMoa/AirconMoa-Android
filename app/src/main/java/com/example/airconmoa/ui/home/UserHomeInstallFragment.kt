89ba6d6dbb3fabd8680bb897c0793cf54b4301ce
hhj <sbs98010@naver.com>
2023-11-07 15:58:09 +0900

    Update : 사용자 메인화면->견적요청서 액티비티 연결

--- app/src/main/java/com/example/airconmoa/ui/home/UserHomeInstallFragment.kt
+++ app/src/main/java/com/example/airconmoa/ui/home/UserHomeInstallFragment.kt
@@ -1,11 +1,13 @@
 package com.example.airconmoa.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.airconmoa.R
 import com.example.airconmoa.config.BaseFragmentVB
 import com.example.airconmoa.databinding.FragmentUserHomeInstallBinding
+import com.example.airconmoa.ui.estimate_user.UserEstimateActivity
 
 
 class UserHomeInstallFragment : BaseFragmentVB<FragmentUserHomeInstallBinding>(FragmentUserHomeInstallBinding::bind, R.layout.fragment_user_home_install) {
@@ -28,6 +30,11 @@ class UserHomeInstallFragment : BaseFragmentVB<FragmentUserHomeInstallBinding>(F
         }
         binding.rvMain.adapter= CompanyItemDataAdapter(dataList)
         binding.rvMain.layoutManager= LinearLayoutManager(context)
+
+        binding.estimateWriteBtn.setOnClickListener {
+            val intent= Intent(context,UserEstimateActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
