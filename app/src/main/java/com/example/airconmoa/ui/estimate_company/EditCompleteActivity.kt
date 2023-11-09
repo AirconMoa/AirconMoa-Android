7c7da33fb2e35271703f7201536d2d42a65268fe
hhj <sbs98010@naver.com>
2023-11-09 15:00:12 +0900

    메인에서~끝까지 2/2

---
+++ app/src/main/java/com/example/airconmoa/ui/estimate_company/EditCompleteActivity.kt
@@ -0,0 +1,14 @@
+package com.example.airconmoa.ui.estimate_company
+
+import android.os.Bundle
+import com.example.airconmoa.config.BaseActivityVB
+import com.example.airconmoa.databinding.ActivityEditCompleteBinding
+
+class EditCompleteActivity : BaseActivityVB<ActivityEditCompleteBinding>(ActivityEditCompleteBinding::inflate){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+    }
+}
