bba1e98863e6c920ead8c0ae98815aa3327fb690
hhj <sbs98010@naver.com>
2023-11-16 00:35:23 +0900

    #24 프레임 레이아웃 범위를 하단 버튼도 포함하게끔 수정

--- app/src/main/java/com/example/airconmoa/ui/estimate_user/UserEstimateActivity.kt
+++ app/src/main/java/com/example/airconmoa/ui/estimate_user/UserEstimateActivity.kt
@@ -1,9 +1,6 @@
 package com.example.airconmoa.ui.estimate_user
 
-import android.animation.ObjectAnimator
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.airconmoa.config.BaseActivityVB
 import com.example.airconmoa.databinding.ActivityUserEstimateBinding
@@ -35,7 +32,7 @@ class UserEstimateActivity: BaseActivityVB<ActivityUserEstimateBinding>(Activity
                 .replace(userEstimateFrame.id, UserEstimateFragment())
                 .commitAllowingStateLoss()
 
-            btnNext.setOnClickListener {
+            /*btnNext.setOnClickListener {
                 if(counter<=6) {
                     //약간 어색.. 끝날때 애니메이션 추가해야할듯
                     ObjectAnimator.ofFloat(userEstimateFrame, "translationX", 1000f, 0f).apply {
@@ -51,7 +48,8 @@ class UserEstimateActivity: BaseActivityVB<ActivityUserEstimateBinding>(Activity
                         Log.d("확인 ","{$counter}")
                     }
                     Log.d("확인2 ","{$counter}")
-
+                    estimateProgressText.text="$counter/7"
+                    estimateProgress.setProgress(counter)
                 }
                 else if(counter >= 7){
                     //success 이동
@@ -61,8 +59,10 @@ class UserEstimateActivity: BaseActivityVB<ActivityUserEstimateBinding>(Activity
                     Log.d("확인 ","{$counter}")
                 }
             }
+             */
 
         }
+
 
 
 
